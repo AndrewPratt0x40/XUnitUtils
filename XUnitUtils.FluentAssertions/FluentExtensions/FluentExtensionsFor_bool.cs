58fd40a4f7f08a6aca7dc76a26d76fbf154f75fd
andrewpratt0x40 <andrewpratt0x40@gmail.com>
2023-10-02 14:03:23 -0400

    Implemented FluentExtensionsFor_bool.BeTrueIFF and FluentExtensionsFor_bool.BeFalseIFF with passing unit tests.

---
+++ XUnitUtils.FluentAssertions/FluentExtensions/FluentExtensionsFor_bool.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using FluentAssertions.Primitives;
+
+namespace XUnitUtils.FluentAssertions.FluentExtensions
+{
+	/// <summary>
+	/// Contains fluent extensions for instances of the type <see cref="bool"/>.
+	/// </summary>
+	public static class FluentExtensionsFor_bool
+	{
+		/// <summary>
+		/// Asserts that the value is <see langword="true"/> if and only if another value is <see langword="true"/>.
+		/// </summary>
+		/// <param name="instance">The value to assert.</param>
+		/// <param name="other">The value to compare with.</param>
+		/// <param name="because"><inheritdoc cref="BooleanAssertions{TAssertions}.Be(bool, string, object[])" path="//param[@name='because']"/></param>
+		/// <param name="becauseArgs"><inheritdoc cref="BooleanAssertions{TAssertions}.Be(bool, string, object[])" path="//param[@name='becauseArgs']"/></param>
+		public static AndConstraint<BooleanAssertions> BeTrueIFF(this BooleanAssertions instance, bool other, string because = "", params object[] becauseArgs)
+		{
+			if (other)
+				return instance.BeTrue(because, becauseArgs);
+
+			return instance.BeFalse(because, becauseArgs);
+		}
+
+
+		/// <summary>
+		/// Asserts that the value is <see langword="false"/> if and only if another value is <see langword="true"/>.
+		/// </summary>
+		/// <param name="instance">The value to assert.</param>
+		/// <param name="other">The value to compare with.</param>
+		/// <param name="because"><inheritdoc cref="BooleanAssertions{TAssertions}.Be(bool, string, object[])" path="//param[@name='because']"/></param>
+		/// <param name="becauseArgs"><inheritdoc cref="BooleanAssertions{TAssertions}.Be(bool, string, object[])" path="//param[@name='becauseArgs']"/></param>
+		public static AndConstraint<BooleanAssertions> BeFalseIFF(this BooleanAssertions instance, bool other, string because = "", params object[] becauseArgs)
+		{
+			if (other)
+				return instance.BeFalse(because, becauseArgs);
+
+			return instance.BeTrue(because, becauseArgs);
+		}
+	}
+}
---
+++ XUnitUtils.FluentAssertionsTests/Tests/FluentExtensions/TestsFor_FluentExtensionsFor_bool.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit.Sdk;
+using XUnitUtils.FluentAssertions.FluentExtensions;
+
+namespace XUnitUtils.FluentAssertionsTests.Tests
+{
+	public class TestsFor_FluentExtensionsFor_bool
+	{
+		#region Tests
+
+		[Theory]
+		[InlineData(false, true)]
+		[InlineData(true, false)]
+		public void BeTrueIFF_with_unsatisfactory_values_fails(bool value, bool otherValue)
+		{
+			// Arrange
+			Action action = () => value.Should().BeTrueIFF(otherValue);
+			
+			// Assert
+			action.Should().Throw<XunitException>();
+		}
+
+
+		[Theory]
+		[InlineData(false, false)]
+		[InlineData(true, true)]
+		public void BeTrueIFF_with_satisfactory_values_passes(bool value, bool otherValue)
+		{
+			// Arrange
+			Action action = () => value.Should().BeTrueIFF(otherValue);
+
+			// Assert
+			action.Should().NotThrow();
+		}
+
+
+		[Theory]
+		[InlineData(false, false)]
+		[InlineData(true, true)]
+		public void BeFalseIFF_with_unsatisfactory_values_fails(bool value, bool otherValue)
+		{
+			// Arrange
+			Action action = () => value.Should().BeFalseIFF(otherValue);
+
+			// Assert
+			action.Should().Throw<XunitException>();
+		}
+
+
+		[Theory]
+		[InlineData(false, true)]
+		[InlineData(true, false)]
+		public void BeFalseIFF_with_satisfactory_values_passes(bool value, bool otherValue)
+		{
+			// Arrange
+			Action action = () => value.Should().BeFalseIFF(otherValue);
+
+			// Assert
+			action.Should().NotThrow();
+		}
+
+		#endregion
+	}
+}
