34037923b815a8aed6d02c232cfef5a8b4a61ac1
Andrew Pratt <andrewpratt0x40@gmail.com>
2023-10-06 13:16:28 -0400

    1.0.0-alpha.2 (#3)
    Implemented EnumerableGenerator.

--- XUnitUtils/TheoryData/Theory data item generation/EnumerableGenerator.cs
+++ XUnitUtils/TheoryData/Theory data item generation/EnumerableGenerator.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,17 +12,67 @@ namespace XUnitUtils.TheoryData.ItemGenerators
 	/// Generates theory data items of collections.
 	/// </summary>
 	/// <typeparam name="TItem">The type of each item in the collections to generate.</typeparam>
-	public class EnumerableGenerator<TItem> : ITheoryDataItemGenerator<IEnumerable<TItem>>
+	/// <typeparam name="TInnerGenerator">The type to generate values of type <typeparamref name="TItem"/>.</typeparam>
+	public class EnumerableGenerator<TItem, TInnerGenerator> : ITheoryDataItemGenerator<IEnumerable<TItem>>
+		where TInnerGenerator : ITheoryDataItemGenerator<TItem>
 	{
 		/// <inheritdoc/>
 		public static IEnumerable<TItem> GetAnyValue() =>
-			throw new NotImplementedException()
+			Enumerable.Empty<TItem>()
 		;
 
 
 		/// <inheritdoc/>
-		public static IEnumerable<IEnumerable<TItem>> GetUniqueValues(int numberOfItems = 2) =>
-			throw new NotImplementedException()
+		public static IEnumerable<IEnumerable<TItem>> GetUniqueValues(int numberOfValues = 2)
+		{
+			if (numberOfValues < 0)
+				throw new ArgumentOutOfRangeException($"Cannot generate {numberOfValues} items. Parameter {nameof(numberOfValues)} must be non-negative.");
+
+			IEnumerable<IEnumerable<TItem>> values = Enumerable.Empty<IEnumerable<TItem>>();
+
+			int numberOfInnerValues = 0;
+			while (values.Count() < numberOfValues)
+				values = values.Concat(GeneratePermutations(TInnerGenerator.GetUniqueValues(numberOfInnerValues++), numberOfValues - values.Count()));
+
+			return values;
+		}
+
+
+		private static IEnumerable<IEnumerable<TItem>> GeneratePermutations(IEnumerable<TItem> items, int maxPermutations) =>
+			items.Any()
+				? GeneratePermutationsStep(items.Count(), items.ToList(), Enumerable.Empty<IEnumerable<TItem>>(), maxPermutations)
+				: Enumerable.Empty<IEnumerable<TItem>>()
 		;
+		
+		
+		private static IEnumerable<IEnumerable<TItem>> GeneratePermutationsStep(int k, IList<TItem> items, IEnumerable<IEnumerable<TItem>> generatedPermutations, int maxPermutations)
+		{
+			Debug.Assert(k >= 1);
+			Debug.Assert(maxPermutations >= 0);
+			Debug.Assert(generatedPermutations.Count() <= maxPermutations);
+			
+			if (generatedPermutations.Count() < maxPermutations)
+			{
+				// Permutations are generated using Heap's algorithm: https://en.wikipedia.org/wiki/Heap%27s_algorithm
+
+				if (k == 1)
+					return generatedPermutations.Append(items);
+
+				generatedPermutations = GeneratePermutationsStep(k - 1, items, generatedPermutations, maxPermutations);
+
+				bool isKEven = k % 2 == 0;
+				for (int i = 0; i < k - 1; i++)
+				{
+					if (isKEven)
+						(items[i], items[k - 1]) = (items[k - 1], items[i]);
+					else
+						(items[0], items[k - 1]) = (items[k - 1], items[0]);
+
+					generatedPermutations = GeneratePermutationsStep(k - 1, items, generatedPermutations, maxPermutations);
+				}
+			}
+
+			return generatedPermutations;
+		}
 	}
 }
---
+++ XUnitUtilsTests/Tests/TheoryData/Theory data item generation/TestsFor_EnumerableGenerator.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using XUnitUtils.TheoryData.ItemGenerators;
+
+namespace XUnitUtilsTests.Tests
+{
+	public class TestsFor_EnumerableGenerator
+	{
+		#region Generic method wrappers
+
+		public static object? Invoke_GetAnyValue(Type TValue, Type TInnerGenerator) =>
+			typeof(EnumerableGenerator<,>)
+			.MakeGenericType(TValue, TInnerGenerator)
+			.GetMethod("GetAnyValue")!
+			.Invoke(null, null)
+		;
+
+
+		public static object? Invoke_GetUniqueValues(Type TValue, Type TInnerGenerator, int numberOfValues) =>
+			typeof(EnumerableGenerator<,>)
+			.MakeGenericType(TValue, TInnerGenerator)
+			.GetMethod("GetUniqueValues")!
+			.Invoke(null, new object?[] { numberOfValues })
+		;
+
+		#endregion
+
+
+		#region Theory data
+
+		public static IEnumerable<(Type TValue, Type TInnerGenerator)> ValidTypes =>
+			new (Type, Type)[]
+			{
+				(typeof(bool), typeof(BoolGenerator)),
+				(typeof(object), typeof(DefaultReferenceTypeGenerator<object>)),
+				(typeof(TestsFor_EnumGenerator.EEnumWith3Values), typeof(EnumGenerator<TestsFor_EnumGenerator.EEnumWith3Values>)),
+				(typeof(int), typeof(NumericGenerator<int>)),
+				(typeof(uint), typeof(NumericGenerator<uint>)),
+				(typeof(float), typeof(NumericGenerator<float>)),
+				(typeof(string), typeof(StringGenerator)),
+				(typeof(IEnumerable<object>), typeof(EnumerableGenerator<object, DefaultReferenceTypeGenerator<object>>))
+			}
+		;
+
+
+		public static IEnumerable<int> ValidNumbersOfValues =>
+			Enumerable.Range(0, 2)
+		;
+		
+		
+		public static TheoryData<Type, Type> TheoryDataFor_GetAnyValue
+		{
+			get
+			{
+				TheoryData<Type, Type> theoryData = new();
+
+				foreach ((Type tValue, Type tInnerGenerator) in ValidTypes)
+					theoryData.Add(tValue, tInnerGenerator);
+
+				return theoryData;
+			}
+		}
+
+
+		public static TheoryData<Type, Type, int> TheoryDataFor_GetUniqueValues
+		{
+			get
+			{
+				TheoryData<Type, Type, int> theoryData = new();
+
+				foreach ((Type tValue, Type tInnerGenerator) in ValidTypes)
+				{
+					foreach (int numberOfValues in ValidNumbersOfValues)
+						theoryData.Add(tValue, tInnerGenerator, numberOfValues);
+				}
+
+				return theoryData;
+			}
+		}
+
+
+		public static TheoryData<Type, Type, int> TheoryDataFor_GetUniqueValues_with_negative_size
+		{
+			get
+			{
+				TheoryData<Type, Type, int> theoryData = new();
+
+				foreach ((Type tValue, Type tInnerGenerator) in ValidTypes)
+				{
+					theoryData.Add(tValue, tInnerGenerator, -1);
+					theoryData.Add(tValue, tInnerGenerator, -2);
+				}
+
+				return theoryData;
+			}
+		}
+
+		#endregion
+
+
+		#region Tests
+
+		[Theory]
+		[MemberData(nameof(TheoryDataFor_GetAnyValue))]
+		public void GetAnyValue_returns_a_valid_value_of_the_expected_type(Type TValue, Type TInnerGenerator)
+		{
+			// Arrange
+			object? returnValue;
+
+			// Act
+			returnValue = Invoke_GetAnyValue(TValue, TInnerGenerator);
+
+			// Assert
+			returnValue.Should().BeAssignableTo(typeof(IEnumerable<>).MakeGenericType(TValue));
+		}
+
+
+		[Theory]
+		[MemberData(nameof(TheoryDataFor_GetUniqueValues))]
+		public void GetUniqueValues_returns_a_collection_of_unique_values_of_the_correct_size(Type TValue, Type TInnerGenerator, int numberOfValues)
+		{
+			// Arrange
+			object? returnValue;
+			IEnumerable<object>? returnValueAsEnumerable;
+			Type expectedReturnType = typeof(IEnumerable<>).MakeGenericType(typeof(IEnumerable<>).MakeGenericType(TValue));
+
+			// Act
+			returnValue = Invoke_GetUniqueValues(TValue, TInnerGenerator, numberOfValues);
+
+			// Assert
+			returnValue.Should().BeAssignableTo
+			(
+				expectedReturnType,
+				$"invoking GetUniqueValues with type parameter {TValue.Name} and generator {TInnerGenerator.Name} should return an instance of type {expectedReturnType.Name}"
+			);
+			returnValueAsEnumerable = (returnValue as IEnumerable)?.Cast<object>();
+			returnValueAsEnumerable.Should().NotBeNull
+			(
+				$"invoking GetUniqueValues with type parameter {TValue.Name} and generator {TInnerGenerator.Name} should return a value that is convertible to {nameof(IEnumerable<object>)}"
+			);
+			returnValueAsEnumerable
+				.Should()
+				.OnlyHaveUniqueItems()
+				.And
+				.HaveCount(numberOfValues)
+			;
+		}
+
+
+		[Theory]
+		[MemberData(nameof(TheoryDataFor_GetUniqueValues_with_negative_size))]
+		public void GetUniqueValues_throws_ArgumentOutOfRangeException_when_numberOfValues_is_negative(Type TValue, Type TInnerGenerator, int numberOfValues)
+		{
+			// Arrange
+			Action action = () => Invoke_GetUniqueValues(TValue, TInnerGenerator, numberOfValues);
+
+			// Assert
+			action.Should().Throw<TargetInvocationException>().WithInnerException<ArgumentOutOfRangeException>();
+		}
+
+		#endregion
+	}
+}
